e2d036838c25a6218627daa042023ccc76892c2e
peterjoho <peterjoho@gmail.com>
2014-02-07 12:29:08 -0500

    This is an R Function for extracting 95% confidence intervals from lmer and glmer objects. Y is a ranef object and Z is a se.ranef object. Dependencies include arm and lme4.

---
+++ lmerSE.R
@@ -0,0 +1,17 @@
+lmerSE <- function(Y,Z){
+  frame <- matrix(nrow = nrow(Y), ncol = 2*ncol(Z))
+  for(j in 1:ncol(Y)){
+    for(i in 1:nrow(Z)){
+      frame[i,j] <- -2*Z[i,j] + Y[i,j]
+      frame[i,j+ncol(Y)] <- 2*Z[i,j] + Y[i,j]
+    }
+  }
+  frame <- as.data.frame(frame)
+  for(k in 1:ncol(Y)){
+    col.name <- colnames(Y)[k]
+    colnames(frame)[k] <- paste0(col.name, ".", "lo")
+    colnames(frame)[k + ncol(Y)] <- paste0(col.name, ".", "hi")
+  }
+  frame <- cbind(Y, frame)
+  return(frame)
+}
